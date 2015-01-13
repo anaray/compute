1b35e685e818ca6d8f2ec89714ae96fa1dc83788
Anumodh Narayanan <anumodh@gmail.com>
2015-01-13 13:43:21 +0530

    cleaning compute

--- compute.go
+++ compute.go
@@ -10,8 +10,7 @@ type Args struct {
 	Incoming chan Packet
 	Outgoing chan Packet
 	WaitGroup *sync.WaitGroup
-	//Done chan bool
-	Container map[string]interface{}
+	Container *map[string]interface{}
 	Logger    *Log
 }
 
@@ -23,29 +22,23 @@ type Computes interface {
 type Packet map[string]interface{}
 
 func Run(computes ...Computes) {
-	//done := make()
 	in := make(chan Packet, 10000)
 	logger := Logger(os.Stdout)
 	var wg sync.WaitGroup
 	var arg Args
 	var args  []Args
 	logger.Logf("Initializing Compute ...")
-	//done := make(chan bool)
-	var indx = 1
 	for i := 0; i < len(computes); i++ {
 		out := make(chan Packet, 10000)
 		arg = Args{Incoming: in, Outgoing: out, Logger: logger, WaitGroup: &wg}
 		args = append(args, arg)
 		in = out
-		indx += 1	
 	}
-
 	for indx, compute := range computes {
 		arg.WaitGroup.Add(1)
 		logger.Logf("Initializing Compute: %s", compute.String())
 		go compute.Execute(&args[indx])
 	}
-
 	arg.WaitGroup.Wait()
 }
 
