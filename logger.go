d46a70e12bb462056aae6ce6f7c94e12b0544f9e
Anumodh Narayanan <anumodh@gmail.com>
2015-03-16 09:43:13 +0530

    fixing compile issue

--- logger.go
+++ logger.go
@@ -14,6 +14,6 @@ func Logger(out io.Writer) (l *Log) {
 	return &Log{log: log.New(out, "[mule] ", log.Ldate|log.Ltime|log.Lmicroseconds)}
 }
 
-func (l *Log) logf(f string, args ...interface{}) {
+func (l *Log) Logf(f string, args ...interface{}) {
 	l.log.Output(2, fmt.Sprintf(f, args...))
 }
